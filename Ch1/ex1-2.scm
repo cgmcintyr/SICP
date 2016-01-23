9550ec256052e3703b2869b606b973937709d69c
Christopher McIntyre <chrsintyre@gmail.com>
2016-01-23 12:01:33 +0000

    Exercise 1.2 completed

---
+++ Ch1/ex1-2.scm
@@ -0,0 +1,29 @@
+;;; SICP Exercise 1.2 Solution
+;;; Written by Christopher McIntyre (chrsintyre)
+;;;
+;;; Define a procedure which takes three numbers as arguments and returns the 
+;;; sum of the squares of the two largest numbers.
+;;;
+
+(define
+  (largest-two-squares-sum x y z)
+    (if (= x (larger x y))
+      (sum-of-squares x (larger y z))
+      (sum-of-squares y (larger x z))
+    )
+)
+
+(define
+  (larger x y)
+    (if (> x y) x y)
+)
+
+(define
+  (sum-of-squares x y)
+    (+ (square x) (square y))
+)
+
+(define
+  (square x)
+    (* x x)
+)
